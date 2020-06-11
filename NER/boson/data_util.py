ffe55bbc9093c3729a1eacd8b2a1d2f2c3cddfc5
灵成 <chaves.lcw@alibaba-inc.com>
2020-06-11 11:45:23 +0800

    兼容python3

--- NER/boson/data_util.py
+++ NER/boson/data_util.py
@@ -7,6 +7,18 @@ import pandas as pd
 import numpy as np
 import re
 
+
+
+import collections
+def flatten(x):
+    result = []
+    for el in x:
+        if isinstance(x, collections.Iterable) and not isinstance(el, str):
+            result.extend(flatten(el))
+        else:
+            result.append(el)
+    return result
+
 def data2pkl():
     datas = list()
     labels = list()
@@ -34,7 +46,6 @@ def data2pkl():
     input_data.close()
     print(len(datas),tags)
     print(len(labels))
-    from compiler.ast import flatten
     all_words = flatten(datas)
     sr_allwords = pd.Series(all_words)
     sr_allwords = sr_allwords.value_counts()
@@ -124,7 +135,7 @@ def origin2tag():
 def tagsplit():
     with open('./wordtag.txt','rb') as inp:
 	    texts = inp.read().decode('utf-8')
-    sentences = re.split('[，。！？、‘’“”（）]/[O]'.decode('utf-8'), texts)
+    sentences = re.split('[，。！？、‘’“”（）]/[O]', texts)
     output_data = codecs.open('./wordtagsplit.txt','w','utf-8')
     for sentence in sentences:
 	    if sentence != " ":
